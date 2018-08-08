0c2807a380db875bea740ac2851a22c4af53dcfb
pSox <psox@st8l.com>
2018-08-08 20:21:23 +0100

    WIP 2018-08-08T20:21:22+01:00

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ extern crate serde_yaml;
 
 fn main() {
     let mut settings = config::Config::default();
-    settings.merge(config::File::with_name("Settings")).unwrap().merge(config::Environment::with_prefix("DYNYAML")).unwrap();
+    settings
+        .merge(config::File::with_name("Settings"))
+        .unwrap()
+        .merge(config::Environment::with_prefix("DYNYAML"))
+        .unwrap();
     let yaml_doc = settings.try_into::<serde_yaml::Value>().unwrap();
-    let yaml = serde_yaml::to_string(&yaml_doc).unwrap(); 
+    let yaml = serde_yaml::to_string(&yaml_doc).unwrap();
     let port = yaml_doc.get("port").unwrap();
     println!("{:#?}", yaml_doc);
     println!("{}", yaml);
