1fe56b2fdd6c51bc6ed5365dd7641b51cd4498c6
pSox <psox@st8l.com>
2018-08-14 20:19:17 +0100

    It Works !!!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate config;
-extern crate serde_yaml;
 extern crate serde_json;
+extern crate serde_yaml;
 #[macro_use]
 extern crate clap;
 
@@ -10,82 +10,89 @@ use std::path::Path;
 
 use clap::{App, Arg};
 
-fn writer(o: &str, f: &[u8]) -> std::io::Result<()> {
-    let mut file = File::create(o)?;
-    file.write_all(f)?;
-    Ok(())
+fn writer(file_name: &str, output_data: &[u8]) {
+    let mut file = File::create(file_name).unwrap();
+    file.write_all(output_data).unwrap();
 }
 
 fn main() {
     let matches = App::new("Trope")
-                        .version(crate_version!())
-                        .author(crate_authors!("/n"))
-                        .about("Utility for merging YAML and JSON files")
-                        .arg(Arg::with_name("output")
-                                    .short("O")
-                                    .long("output")
-                                    .help("Sets the path to the output file")
-                                    .takes_value(true))
-                        .arg(Arg::with_name("config")
-                                    .short("c")
-                                    .long("config")
-                                    .value_name("FILE")
-                                    .help("Specifies file(s) to merge")
-                                    .takes_value(true)
-                                    .multiple(true)
-                                    .required(true))
-                        .arg(Arg::with_name("env_var")
-                                    .short("e")
-                                    .long("env")
-                                    .help("Merges enviromental variables with prefix: TROPE"))            
-                        .arg(Arg::with_name("debug")
-                                    .short("d")
-                                    .long("debug")
-                                    .help("Displays contents of file(s) from 'config'"))
-                        .get_matches();
+        .version(crate_version!())
+        .author(crate_authors!("/n"))
+        .about("Utility for merging YAML and JSON files")
+        .arg(
+            Arg::with_name("output")
+                .short("O")
+                .long("output")
+                .help("Sets the path to the output file")
+                .takes_value(true),
+        ).arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Specifies file(s) to merge")
+                .takes_value(true)
+                .multiple(true)
+                .required(true),
+        ).arg(
+            Arg::with_name("env_var")
+                .short("e")
+                .long("env")
+                .help("Merges environmental variables with prefix: TROPE"),
+        ).arg(
+            Arg::with_name("debug")
+                .short("d")
+                .long("debug")
+                .help("Displays contents of file(s) from 'config'"),
+        ).get_matches();
 
     let mut settings = config::Config::default();
 
     let in_iterator = matches.values_of("config");
     for f in in_iterator.unwrap() {
-    settings
-        .merge(config::File::with_name(f))
-        .expect("Error in merging given files");
+        settings
+            .merge(config::File::with_name(f))
+            .expect("Error in merging given files");
     }
 
     if matches.is_present("env_var") {
-    settings
-        .merge(config::Environment::with_prefix("TROPE"))
-        .expect("Error in merging given environmental variable");
+        settings
+            .merge(config::Environment::with_prefix("TROPE"))
+            .expect("Error in merging given environmental variable");
     }
 
     if let Some(o) = matches.value_of("output") {
         let file_path = Path::new(o);
         let extension_type = match file_path.extension() {
-        None => "",
-        Some(os_str) => {
-            match os_str.to_str() {
+            None => "",
+            Some(os_str) => match os_str.to_str() {
                 Some("yaml") => "yaml",
                 Some("yml") => "yaml",
                 Some("json") => "json",
                 _ => panic!("Can't find extension type"),
-                }
-            }
+            },
         };
         if extension_type == "yaml" {
-            let yaml_doc = settings.try_into::<serde_yaml::Value>().expect("Error in serializing merged configs");
-            let yaml = serde_yaml::to_string(&yaml_doc).expect("Error in moving merged configs to string");
+            let yaml_doc = settings
+                .try_into::<serde_yaml::Value>()
+                .expect("Error in serializing merged configs");
+            let yaml =
+                serde_yaml::to_string(&yaml_doc).expect("Error in moving merged configs to string");
             writer(o, yaml.as_bytes());
         } else if extension_type == "json" {
-            let json_doc = settings.try_into::<serde_json::Value>().expect("Error in serializing merged configs");
-            let json = serde_json::to_string(&json_doc).expect("Error in moving merged configs to string");
+            let json_doc = settings
+                .try_into::<serde_json::Value>()
+                .expect("Error in serializing merged configs");
+            let json =
+                serde_json::to_string(&json_doc).expect("Error in moving merged configs to string");
             writer(o, json.as_bytes());
         }
     }
 
     if matches.is_present("debug") {
-    //println!("{:#?}", yaml_doc);
-    //println!("{}", yaml);
-    //println!("{:#?}", port);
+        //println!("{:#?}", yaml_doc);
+        //println!("{}", yaml);
+        //println!("{:#?}", port);
     }
 }
